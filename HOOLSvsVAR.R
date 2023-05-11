e20971ae4c22316f9cdcad84c0d65a1bbf628b00
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2023-05-11 15:16:09 +0200

    finalize HOOLSvsVAR

--- HOOLSvsVAR.R
+++ HOOLSvsVAR.R
@@ -1,12 +1,13 @@
 # Testing HOOLS vs VAR
 library(TensorEconometrics)
 library(dplyr)
+library(vars)
 
 var_ols <- function(A){
   dimensions_A <- dim(A)
-  original_A <- A[-1,]
-  lagged_A <- A[1:(dimensions_A[1]-1),]
-  ols_hat <- solve(t(lagged_A)%*%lagged_A) %*% (t(lagged_A)%*%original_A)
+  response <- A[-1,]
+  predictor <- A[1:(dimensions_A[1]-1),]
+  ols_hat <- solve(t(predictor)%*%predictor) %*% (t(predictor)%*%response)
   return(t(ols_hat))
 }
 
@@ -28,3 +29,13 @@ var_parameters <- var_ols(demeaned_traditional_data)
 predictor_tensor <- demeaned_tensor_data@data[1:160,,]
 response_tensor <- demeaned_tensor_data@data[2:161,,]
 HOOLS_parameters <- HOOLS(as.tensor(response_tensor), as.tensor(predictor_tensor), 1, 1)
+
+# Compare HOOLS and VAR
+# The first matrix of HOOLS parameter should line up with the first row of the VAR parameter
+# Note I don't use the unfold function, but rather the matrix function in order to obtain the correct
+# parameters.
+
+HOOLS_parameters@data[,,1,1] # Corresponds to the first row of the VAR parameter, [,,1,2] corresponds 
+# to the second row
+
+unfolded_HOOLS <- unfold(HOOLS_parameters, c(4,3), c(2,1))@data
