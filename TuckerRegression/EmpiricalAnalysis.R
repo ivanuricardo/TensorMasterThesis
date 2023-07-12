fd89601e9082e9e06e1d612c48214354b03b6e70
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2023-07-12 13:57:43 +0200

    covariance edit

--- TuckerRegression/EmpiricalAnalysis.R
+++ TuckerRegression/EmpiricalAnalysis.R
@@ -3,10 +3,10 @@ library(tidyverse)
 set.seed(20230706)
 
 data("tensor_data")
-countries <- c("ARG", "AUS", "AUT", "BEL", "BRA", "CAN", "CHN", "CHL", "FIN", 
-               "FRA", "DEU", "IND", "IDN", "ITA", "JPN", "KOR", "MYS", "MEX", 
-               "NLD", "NOR", "NZL", "PER", "PHL", "ZAF", "SGP", "ESP", "SWE",
-               "CHE", "THA", "TUR", "GBR", "USA")
+countries <- c("AUS", "AUT", "BEL", "CAN", "CHN", "CHL", "FIN", 
+               "FRA", "DEU", "IND", "IDN", "ITA", "JPN", "KOR", "MYS", 
+               "NLD", "NOR", "NZL", "PHL", "ZAF", "SGP", "ESP", "SWE",
+               "CHE", "THA", "GBR", "USA")
 econind <- c("y", "Dp", "r")
 
 # Demean both data sets
@@ -15,28 +15,20 @@ array_means <- array(tensor_means, dim = c(32,3,161)) %>%
   aperm(c(3,1,2))
 demeaned_tensor_data <- as.tensor(tensor_data - array_means)
 
+# Remove Argentina, Brazil, Mexico, Peru, and Turkey
+tdata <- demeaned_tensor_data@data[, -c(1, 5, 18, 22, 30), ]
+
 # Lag split
-predictor_tensor <- demeaned_tensor_data@data[1:160,,]
-response_tensor <- demeaned_tensor_data@data[2:161,,]
+predictor_tensor <- tdata[1:160,,]
+response_tensor <- tdata[2:161,,]
 HOOLS_parameters <- HOOLS(as.tensor(response_tensor), as.tensor(predictor_tensor), 1, 1)
 
 # Tucker Rank Selection
 tucker_rank_selection(HOOLS_parameters, 160)
 
-# Check for a variety of observations?
-for (i in 1:100) {
-  print(tucker_rank_selection(HOOLS_parameters, i))
-}
-
-# Or for a variety of chosen c?
-c_ops <- 0.01*1:99
-for (i in c_ops) {
-  print(tucker_rank_selection(HOOLS_parameters, c = i, num_obs = 160))
-}
-
-# Estimate Tucker Regression using c(7,2,5,2)
+# Estimate Tucker Regression using c(7,3,5,3)
 tuckerReg <- tucker_regression(as.tensor(response_tensor), as.tensor(predictor_tensor),
-                               R = c(7,2,5,2), max_iter = 1000)
+                               R = c(7,3,5,3), max_iter = 1000)
 
 # Data frames
 f1 <- data.frame(tuckerReg$factors[[2]])
@@ -44,16 +36,16 @@ f2 <- data.frame(tuckerReg$factors[[3]])
 f3 <- data.frame(tuckerReg$factors[[4]])
 f4 <- data.frame(tuckerReg$factors[[5]])
 
-colnames(f1) <- c("F1", "F2", "F3", "F4", "F5", "F6", "F7")
-colnames(f3) <- c("F1", "F2", "F3", "F4", "F5")
-colnames(f2) <- c("F1", "F2")
-colnames(f4) <- c("F1", "F2")
+colnames(f1) <- paste0("F", seq(1, 7))
+colnames(f3) <- paste0("F", seq(1, 5))
+colnames(f2) <- paste0("F", seq(1, 3))
+colnames(f4) <- paste0("F", seq(1, 3))
 rownames(f1) <- countries
 rownames(f3) <- countries
 rownames(f2) <- econind
 rownames(f4) <- econind
 
-# Define the dataframes
+# Prepare for plotting
 df1 <- f1 %>% 
   rownames_to_column(var = "Country") %>% 
   pivot_longer(cols = -Country, names_to = "Factor", values_to = "Value")
@@ -70,28 +62,29 @@ df4 <- f4 %>%
   rownames_to_column(var = "Econ") %>% 
   pivot_longer(cols = -Econ, names_to = "Factor", values_to = "Value")
 
-color_palette <- c("red", "white", "blue")
+color_palette <- c("#006ddb", "white", "#920000")
+
 # Create heatmap for f1
 ggplot(df1, aes(Factor, Country, fill = Value)) +
   geom_tile() +
   scale_fill_gradientn(colors = color_palette) +
-  labs(title = "Response Variable: Countries", x = "Factors", y = "Countries")
+  labs(title = "Predictor Variables: Country", x = "Factors", y = "Countries")
 
 # Create heatmap for f2
 ggplot(df2, aes(Factor, Econ, fill = Value)) +
   geom_tile() +
   scale_fill_gradientn(colors = color_palette) +
-  labs(title = "Response Variable: Economic Indicators", x = "Factors", y = "Economic Indicators")
+  labs(title = "Predictor Variables: Economic Indicator", x = "Factors", y = "Economic Indicators")
 
 # Create heatmap for f3
 ggplot(df3, aes(Factor, Country, fill = Value)) +
   geom_tile() +
   scale_fill_gradientn(colors = color_palette) +
-  labs(title = "Predictor Variable: Countries", x = "Factors", y = "Countries")
+  labs(title = "Response Variables: Country", x = "Factors", y = "Countries")
 
 # Create heatmap for f4
 ggplot(df4, aes(Factor, Econ, fill = Value)) +
   geom_tile() +
   scale_fill_gradientn(colors = color_palette) +
-  labs(title = "Predictor Variable: Economic Indicators", x = "Factors", y = "Economic Indicators")
+  labs(title = "Response Variables: Economic Indicator", x = "Factors", y = "Economic Indicators")
 
---
+++ TuckerRegression/covarianceAnalysis.R
@@ -0,0 +1,78 @@
+library(tidyverse)
+library(TensorEconometrics)
+library(MixMatrix)
+library(reshape2)
+library(corrplot)
+set.seed(20230706)
+
+data("tensor_data")
+countries <- c("AUS", "AUT", "BEL", "CAN", "CHN", "CHL", "FIN", 
+               "FRA", "DEU", "IND", "IDN", "ITA", "JPN", "KOR", "MYS", 
+               "NLD", "NOR", "NZL", "PHL", "ZAF", "SGP", "ESP", "SWE",
+               "CHE", "THA", "GBR", "USA")
+econind <- c("y", "Dp", "r")
+
+get_lower_tri<-function(x){
+  x[upper.tri(x)] <- NA
+  return(x)
+}
+
+# Demean both data sets
+tensor_means <- apply(tensor_data, MARGIN = c(2,3), mean)
+array_means <- array(tensor_means, dim = c(32,3,161)) %>% 
+  aperm(c(3,1,2))
+demeaned_tensor_data <- as.tensor(tensor_data - array_means)
+
+# Remove Argentina, Brazil, Mexico, Peru, and Turkey
+tdata <- demeaned_tensor_data@data[, -c(1, 5, 18, 22, 30), ]
+
+# Lag split
+predictor_tensor <- tdata[1:160,,]
+response_tensor <- tdata[2:161,,]
+HOOLS_parameters <- HOOLS(as.tensor(response_tensor), as.tensor(predictor_tensor), 1, 1)
+
+# Tucker Regression
+tuckerReg <- tucker_regression(as.tensor(response_tensor), as.tensor(predictor_tensor),
+                               R = c(7,3,5,3), max_iter = 1000)
+
+# Extract Residuals
+Et <- as.tensor(response_tensor) - ttt(as.tensor(predictor_tensor), 
+                                       tuckerReg$B, alongA = c(2,3), alongB = c(1,2))
+permEt <- aperm(Et@data, c(2,3,1))
+
+MatrixEst <- MLmatrixnorm(permEt)
+
+Uest <- MatrixEst$U
+Vest <- MatrixEst$V
+
+colnames(Uest) <- countries
+colnames(Vest) <- econind
+rownames(Uest) <- countries
+rownames(Vest) <- econind
+
+lowerV <- get_lower_tri(Vest)
+meltedV <- melt(lowerV, na.rm = TRUE)
+
+lowerU <- get_lower_tri(Uest)
+meltedU <- melt(lowerU, na.rm = TRUE)
+
+color_palette <- c("white", "red")
+
+ggplot(data = meltedU, aes(x = Var1, y = Var2, fill = value)) + 
+  geom_tile() + 
+  scale_fill_gradientn(colors = color_palette) +
+  labs(title = "Estimated Covariance: Countries", x = "Countries", y = "Countries")
+
+ggplot(data = meltedV, aes(x = Var1, y = Var2, fill = value)) + 
+  geom_tile() + 
+  scale_fill_gradientn(colors = color_palette) +
+  labs(title = "Estimated Covariance: Econ", x = "Indicator", y = "Indicator")
+
+corrplot(cov2cor(Uest), method = "square", tl.col = "black",
+         col = colorRampPalette(c("#006ddb","white","#920000"))(200)) 
+corrplot(cov2cor(Vest), method = "square", tl.col = "black",
+         col = colorRampPalette(c("#006ddb","white","#920000"))(200))
+
+RSS <- innerProd(Et, Et)
+TSS <- innerProd(as.tensor(predictor_tensor), as.tensor(predictor_tensor))
+1-(RSS/TSS)
